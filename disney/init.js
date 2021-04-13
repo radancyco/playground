5c347bb7a57ed431d4b46e0e4fc6d8e51d4fe53b
michaelspellacy <spell@spellacy.net>
2021-04-13 19:59:47 -0400

    Update init.js

--- disney/init.js
+++ disney/init.js
@@ -23,13 +23,13 @@ var $SlickSliderList = $(".one-time");
 
 setTimeout(function(){
 
-	$(".slick-dots").after("<button class='slick-carousel-pause' aria-label='Pause' aria-pressed='false'>Pause</button>");
+	$(".slick-dots").after("<button class='slick-carousel-pause' aria-pressed='false'>Pause</button>");
 
 }, 3000);
 
 $(".slick-carousel-pause").on("click", function() {
 
-  if ($(this).html() === "Pause"){
+  if ($(this).html() == "Pause"){
 
     $SlickSliderList.slick("slickPause");
     $(this).attr("aria-pressed", "true");
