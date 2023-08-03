1dc5b1e9e426563cad289bb931d3c3f07f58d1f7
Michael Spellacy (Spell) <spell@spellacy.net>
2023-08-03 11:12:59 -0400

    form updates

--- product/events/d59fa6b2d07c69e744f688bddfcc8c1c26995fee/init.js
+++ product/events/d59fa6b2d07c69e744f688bddfcc8c1c26995fee/init.js
@@ -1,5 +1,27 @@
-document.getElementById("event-form").addEventListener("submit", function(e) {
 
+// Quick and dirty hidding of content when submit clicked, to illustrate behavior. 
+
+// If multiple forms, will obviously will need forEach.
+
+var formElement = document.getElementById("event-form");
+var formSuccess = document.getElementById("event-success");
+var formSuccessHeading = document.getElementById("success-heading");
+var formSuccessClose = document.getElementById("event-success-btn");
+
+formElement.addEventListener("submit", function(e) {
+
+  formElement.classList.add("success");
+  formSuccess.removeAttribute("hidden");
+  formSuccessHeading.setAttribute("tabindex", "-1");
+  formSuccessHeading.focus();
+  
   e.preventDefault();
+
+});
+
+formSuccessClose.addEventListener("click", function() {
+
+  formElement.classList.remove("success");
+  formSuccess.setAttribute("hidden", "");
 
 });
